1398f02c3abebc7d6c9ae55e6856602a6e1ce89e
Sankalp-G <sankalp.gawali@gmail.com>
2023-01-09 16:38:20 +0530

    add card creation code

--- pages/homepage/script.js
+++ pages/homepage/script.js
@@ -1,3 +1,36 @@
+// creates a new element with given parameters
+function newElement(type_str, attributes_obj, textContent = '') {
+    let element = document.createElement(type_str);
+    element.textContent = textContent;
+
+    for (const [attr, value] of Object.entries(attributes_obj)) {
+        element.setAttribute(attr, value);
+    }
+    return element;
+}
+
+function createCardInfoElement(title_text, subtitle_text) {
+    let info = newElement('div', { class: 'card_info' })
+    let title = newElement('h5', { class: 'card_title' }, title_text)
+    let subtitle = newElement('div', { class: 'card_subtitle' }, subtitle_text)
+
+    info.appendChild(title)
+    info.appendChild(subtitle)
+    return info
+}
+
+function createContentCard(title, subtitle, img_src) {
+    let card = newElement('li', { class: 'content_card' })
+    let img = newElement('img', { src: img_src })
+    let card_info = createCardInfoElement(title, subtitle)
+
+    card.appendChild(img)
+    card.appendChild(card_info)
+    return card
+}
+
+let sections = document.querySelectorAll('.section_container')
+
 // scroll button events
 sections.forEach(section => {
     let left_button = section.querySelector('#left_scroll');
