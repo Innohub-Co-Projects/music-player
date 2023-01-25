4e511a547e50a211954638efde8524fc27d5f14b
Sankalp-G <sankalp.gawali@gmail.com>
2023-01-25 19:42:13 +0530

    add liked songs module

---
+++ modules/like-songs.js
@@ -0,0 +1,34 @@
+// liked songs code:
+// Uses local storage so that liked songs are saved even when window is closed
+
+// returns an array of all song api ids
+function getLikedSongIDs() {
+    let id_str = window.localStorage.liked_song_ids;
+
+    if (id_str == undefined) { return undefined };
+
+    return JSON.parse(window.localStorage.liked_song_ids);
+}
+
+function addLikedSongID(song_api_id) {
+    if (getLikedSongIDs() == undefined) {
+        window.localStorage.liked_song_ids = JSON.stringify([song_api_id]);
+        return;
+    }
+
+    let id_arr = getLikedSongIDs();
+    id_arr.push(song_api_id);
+
+    window.localStorage.liked_song_ids = JSON.stringify(id_arr);
+}
+
+import { fetchSongDetails } from './api-requests.js';
+
+// fetches an array of all liked song data
+async function fetchLikedSongs() {
+    let song_id_string = getLikedSongIDs().join(',');
+
+    return await fetchSongDetails(song_id_string);
+}
+
+export { getLikedSongIDs, addLikedSongID, fetchLikedSongs };
