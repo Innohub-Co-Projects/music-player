715be130e077191608843e908ca894202300857a
Sankalp-G <sankalp.gawali@gmail.com>
2023-02-16 17:30:22 +0530

    refactor and fix formatting

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+import { addLanguage, removeLanguage, getLanguageArray } from "./modules/language.js";
+
 // set correct height for mobile:
 // otherwise navbar is initially hidden on mobile
-function resetHeight(){
+function resetHeight() {
     document.body.style.height = window.innerHeight + "px";
 }
 
@@ -11,335 +13,324 @@ resetHeight();
 
 // Navigation functions:
 
-let iframe = document.querySelector('iframe');
+let iframe = document.querySelector("iframe");
 
 function frameNavigate(url) {
     iframe.src = url;
 }
 
 function navTo(destination) {
-    frameNavigate('./pages/' + destination + '/' + destination + '.html')
+    frameNavigate("./pages/" + destination + "/" + destination + ".html");
 }
 window.navTo = navTo;
 
 function setNavElementActive(nav_element) {
-    document.querySelectorAll('.menu_item').forEach(menu_item => {
-        menu_item.classList.remove('active')
-    })
+    document.querySelectorAll(".menu_item").forEach((menu_item) => {
+        menu_item.classList.remove("active");
+    });
 
-    nav_element.classList.add('active')
+    nav_element.classList.add("active");
 }
 
 function setSectionTitle(title) {
-    let section_title = document.querySelector('#section_title');
+    let section_title = document.querySelector("#section_title");
     section_title.textContent = title;
 }
-window.setSectionTitle = setSectionTitle
+window.setSectionTitle = setSectionTitle;
 
 // set nav element as active and change title when click on a nav element
-document.querySelectorAll('.menu_item').forEach(menu_item => {
-    menu_item.addEventListener('click', (e) => {
-        setNavElementActive(e.target)
-        setSectionTitle(e.target.textContent)
-    })
-})
+document.querySelectorAll(".menu_item").forEach((menu_item) => {
+    menu_item.addEventListener("click", (e) => {
+        setNavElementActive(e.target);
+        setSectionTitle(e.target.textContent);
+    });
+});
 
 // Player functions:
 var music = new Audio();
-let masterPlay = document.getElementById('masterPlay');
-let wave = document.getElementsByClassName('wave')[0];
+let masterPlay = document.getElementById("masterPlay");
+let wave = document.querySelector(".wave");
 
 // set initial song
-playAudio('./assets/rickroll.mp3', 'Never Gonna Give You Up', 'Rick Astley', './assets/rickroll.jpg')
+playAudio("./assets/rickroll.mp3", "Never Gonna Give You Up", "Rick Astley", "./assets/rickroll.jpg");
 
 function playAudio(audio_src, audio_title, audio_subtitle, thumbnail_img) {
     music.src = audio_src;
     music.play();
 
-    let title_element = document.getElementById('title');
+    let title_element = document.getElementById("title");
     title_element.innerHTML = `${audio_title} <br> <div class="subtitle">${audio_subtitle}</div>`;
 
-    let thumbnail = document.getElementById('poster_master_play');
+    let thumbnail = document.getElementById("poster_master_play");
     thumbnail.src = thumbnail_img;
 }
-window.playAudio = playAudio
+window.playAudio = playAudio;
 
 // change icon on play/pause
-music.addEventListener('pause', () => {
-    masterPlay.classList.add('bi-play-fill');
-    masterPlay.classList.remove('bi-pause-fill');
-    wave.classList.remove('active2');
-})
-music.addEventListener('play', () => {
-    masterPlay.classList.remove('bi-play-fill');
-    masterPlay.classList.add('bi-pause-fill');
-    wave.classList.add('active2');
-})
+music.addEventListener("pause", () => {
+    masterPlay.classList.add("bi-play-fill");
+    masterPlay.classList.remove("bi-pause-fill");
+    wave.classList.remove("active2");
+});
 
-masterPlay.addEventListener('click',()=>{
-    if (music.paused || music.currentTime <=0) {
+music.addEventListener("play", () => {
+    masterPlay.classList.remove("bi-play-fill");
+    masterPlay.classList.add("bi-pause-fill");
+    wave.classList.add("active2");
+});
+
+masterPlay.addEventListener("click", () => {
+    if (music.paused || music.currentTime <= 0) {
         music.play();
     } else {
         music.pause();
     }
-} )
+});
 
-let currentStart = document.getElementById('currentStart');
-let currentEnd = document.getElementById('currentEnd');
-let seek = document.getElementById('seek');
-let bar2 = document.getElementById('bar2');
-let dot = document.getElementsByClassName('dot')[0];
+function formatDuration(duration) {
+    let min = Math.floor(duration / 60);
+    let sec = Math.floor(duration % 60);
+    if (sec < 10) { sec = `0${sec}`; }
+    return `${min}:${sec}`;
+}
 
-music.addEventListener('timeupdate',()=>{
+let currentStart = document.getElementById("currentStart");
+let currentEnd = document.getElementById("currentEnd");
+let seek = document.getElementById("seek");
+let bar2 = document.getElementById("bar2");
+let dot = document.getElementsByClassName("dot")[0];
+
+music.addEventListener("timeupdate", () => {
     let music_curr = music.currentTime;
     let music_dur = music.duration;
 
-    let min = Math.floor(music_dur/60);
-    let sec = Math.floor(music_dur%60);
-    if (sec<10) {
-        sec = `0${sec}`
-    }
-    currentEnd.innerText = `${min}:${sec}`;
+    currentEnd.innerText = formatDuration(music_dur);
 
-    let min1 = Math.floor(music_curr/60);
-    let sec1 = Math.floor(music_curr%60);
-    if (sec1<10) {
-        sec1 = `0${sec1}`
-    }
-    currentStart.innerText = `${min1}:${sec1}`;
+    currentStart.innerText = formatDuration(music_curr);
 
-    let progressbar = parseInt((music.currentTime/music.duration)*100);
+    let progressbar = parseInt((music.currentTime / music.duration) * 100);
     seek.value = progressbar;
     let seekbar = seek.value;
     bar2.style.width = `${seekbar}%`;
     dot.style.left = `${seekbar}%`;
-})
+});
 
-seek.addEventListener('change', ()=>{
-    music.currentTime = seek.value * music.duration/100;
-})
+seek.addEventListener("change", () => {
+    music.currentTime = (seek.value * music.duration) / 100;
+});
 
-let vol_icon = document.getElementById('vol_icon');
-let vol = document.getElementById('vol');
-let vol_dot = document.getElementById('vol_dot');
-let vol_bar = document.getElementsByClassName('vol_bar')[0];
-
-vol.addEventListener('change', ()=>{
+vol.addEventListener("change", () => {
+    let vol_icon = document.getElementById("vol_icon");
+    let vol = document.getElementById("vol");
     if (vol.value == 0) {
-        vol_icon.classList.remove('bi-volume-down-fill');
-        vol_icon.classList.add('bi-volume-mute-fill');
-        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.setAttribute('class', 'bi bi-volume-mute-fill');
     }
     if (vol.value > 0) {
-        vol_icon.classList.add('bi-volume-down-fill');
-        vol_icon.classList.remove('bi-volume-mute-fill');
-        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.setAttribute('class', 'bi bi-volume-down-fill');
     }
     if (vol.value > 50) {
-        vol_icon.classList.remove('bi-volume-down-fill');
-        vol_icon.classList.remove('bi-volume-mute-fill');
-        vol_icon.classList.add('bi-volume-up-fill');
+        vol_icon.setAttribute('class', 'bi bi-volume-up-fill');
     }
 
+    let vol_dot = document.getElementById("vol_dot");
+    let vol_bar = document.getElementsByClassName("vol_bar")[0];
     let vol_a = vol.value;
-    vol_bar.style.width = `${vol_a}%`;
     vol_dot.style.left = `${vol_a}%`;
-    music.volume = vol_a/100;
-})
+    vol_bar.style.width = `${vol_a}%`;
+    music.volume = vol_a / 100;
+});
 
-let back = document.getElementById('back');
-let next = document.getElementById('next');
+let back = document.getElementById("back");
+let next = document.getElementById("next");
 
-back.addEventListener('click', ()=>{
-    let iframe_functions = iframe.contentWindow
+back.addEventListener("click", () => {
+    let iframe_functions = iframe.contentWindow;
 
-    iframe_functions.previousSong().play()
-})
+    iframe_functions.previousSong().play();
+});
 
-next.addEventListener('click', ()=>{
-    let iframe_functions = iframe.contentWindow
+next.addEventListener("click", () => {
+    let iframe_functions = iframe.contentWindow;
 
-    iframe_functions.nextSong().play()
-})
+    iframe_functions.nextSong().play();
+});
 
 // list view stuff:
 
 function displayListView(type, api_id) {
-    iframe.addEventListener('load', () => {
-        let frame_functions = iframe.contentWindow
-        if (type == 'playlist') {
-            frame_functions.generateFromPlaylistID(api_id);
-        }
-        else if (type == 'album') {
-            frame_functions.generateFromAlbumID(api_id);
-        }
-        else {
-            console.error('Invalid list view type: ' + type);
-        }
-    }, {once: true})
+    iframe.addEventListener("load", () => {
+            let frame_functions = iframe.contentWindow;
+            if (type == "playlist") {
+                frame_functions.generateFromPlaylistID(api_id);
+            } else if (type == "album") {
+                frame_functions.generateFromAlbumID(api_id);
+            } else {
+                console.error("Invalid list view type: " + type);
+            }
+        },
+        { once: true }
+    );
 
-    iframe.src = './pages/listview/listview.html';
+    iframe.src = "./pages/listview/listview.html";
 }
-window.displayListView = displayListView
+window.displayListView = displayListView;
 
 function displayLikedSongs() {
-    iframe.addEventListener('load', () => {
-        iframe.contentWindow.generateFromLikedSongs()
-    }, {once: true})
+    iframe.addEventListener("load", () => {
+            iframe.contentWindow.generateFromLikedSongs();
+        },
+        { once: true }
+    );
 
-    iframe.src = './pages/listview/listview.html'
+    iframe.src = "./pages/listview/listview.html";
 }
-window.displayLikedSongs = displayLikedSongs
+window.displayLikedSongs = displayLikedSongs;
 
 function searchFor(search_query) {
-    iframe.addEventListener('load', () => {
-        let iframe_functions = iframe.contentWindow
-        iframe_functions.generateSearch(search_query)// FIX SEMICOLONS
-    }, {once: true})
+    iframe.addEventListener("load", () => {
+            let iframe_functions = iframe.contentWindow;
+            iframe_functions.generateSearch(search_query);
+        },
+        { once: true }
+    );
 
-    navTo('search')
+    navTo("search");
 }
-
-function searchSubmitEvent(event) {
-    event.preventDefault()
-    let text_input = event.target.querySelector('input[type="text"]')
-    searchFor(text_input.value)
-    text_input.value = ''
-}
-let form_element = document.querySelector('#search_form')
-form_element.addEventListener('submit', searchSubmitEvent)
 
 function displayLastPlayed() {
-    iframe.addEventListener('load', () => {
-        iframe.contentWindow.generateFromLastPlayed()
-    }, {once: true})
+    iframe.addEventListener("load", () => {
+            iframe.contentWindow.generateFromLastPlayed();
+        },
+        { once: true }
+    );
 
-    iframe.src = './pages/listview/listview.html'
+    iframe.src = "./pages/listview/listview.html";
 }
-window.displayLastPlayed = displayLastPlayed
+window.displayLastPlayed = displayLastPlayed;
+
+function searchSubmitEvent(event) {
+    event.preventDefault();
+    let text_input = event.target.querySelector('input[type="text"]');
+    searchFor(text_input.value);
+    text_input.value = "";
+}
+let form_element = document.querySelector("#search_form");
+form_element.addEventListener("submit", searchSubmitEvent);
 
 // language code:
 
 function toggleLangDropup() {
-    let dropups = document.querySelectorAll('.lang_dropup')
+    let dropups = document.querySelectorAll(".lang_dropup");
 
-    dropups.forEach(dropup => {
-        if (dropup.dataset.state == 'open') {
-            dropup.dataset.state = 'closed';
+    dropups.forEach((dropup) => {
+        if (dropup.dataset.state == "open") {
+            dropup.dataset.state = "closed";
+        } else {
+            dropup.dataset.state = "open";
         }
-        else {
-            dropup.dataset.state = 'open';
-        }
-    })
+    });
 }
 
 // toggle dropup when lang indicator is clicked
-document.querySelector('.lang_indicator span').addEventListener('click', toggleLangDropup)
-
-import { addLanguage, removeLanguage, getLanguageArray } from './modules/language.js'
+document.querySelector(".lang_indicator span").addEventListener("click", toggleLangDropup);
 
 // set initially selected language
-setSelectedLanguages(getLanguageArray())
-updateLangIndicator()
+setSelectedLanguages(getLanguageArray());
+updateLangIndicator();
 
 // set active options in dropup menu
 function setSelectedLanguages(lang_arr) {
-    document.querySelectorAll('.lang_dropup span').forEach(lang_button => {
+    document.querySelectorAll(".lang_dropup span").forEach((lang_button) => {
         if (lang_arr.includes(lang_button.textContent)) {
-            lang_button.dataset.state = 'active'
+            lang_button.dataset.state = "active";
+        } else {
+            lang_button.dataset.state = "inactive";
         }
-        else {
-            lang_button.dataset.state = 'inactive'
-        }
-    })
+    });
 }
 
 function updateLangIndicator() {
-    let lang_indicator = document.querySelector('.lang_indicator span')
-    let languages = getLanguageArray()
+    let lang_indicator = document.querySelector(".lang_indicator span");
+    let languages = getLanguageArray();
     if (languages.length == 0) {
-        lang_indicator.textContent = 'Lang: default'
-    }
-    else if (languages.length == 1) {
-        lang_indicator.textContent = `Lang: ${languages[0]}`
-    }
-    else {
-        lang_indicator.textContent = 'Lang: multi'
+        lang_indicator.textContent = "Lang: default";
+    } else if (languages.length == 1) {
+        lang_indicator.textContent = `Lang: ${languages[0]}`;
+    } else {
+        lang_indicator.textContent = "Lang: multi";
     }
 }
 
 // go to homepage to show changes
 // and display select language in indicator element
 function updateAfterLangChange() {
-    navTo('homepage')
+    navTo("homepage");
 
-    updateLangIndicator()
+    updateLangIndicator();
 }
 
 // add or remove language after clicking on a language option
-document.querySelectorAll('.lang_dropup span').forEach(lang_button => {
-    lang_button.addEventListener('click', () => {
-        if (lang_button.dataset.state == 'active') {
-            lang_button.dataset.state = 'inactive'
-            removeLanguage(lang_button.textContent)
-            updateAfterLangChange()
+document.querySelectorAll(".lang_dropup span").forEach((lang_button) => {
+    lang_button.addEventListener("click", () => {
+        if (lang_button.dataset.state == "active") {
+            lang_button.dataset.state = "inactive";
+            removeLanguage(lang_button.textContent);
+            updateAfterLangChange();
+        } else {
+            lang_button.dataset.state = "active";
+            addLanguage(lang_button.textContent);
+            updateAfterLangChange();
         }
-        else {
-            lang_button.dataset.state = 'active'
-            addLanguage(lang_button.textContent)
-            updateAfterLangChange()
-        }
-    })
-})
+    });
+});
 
 // nav bar dropup stuff:
 
-let more_button = document.querySelector('#more_button')
+let more_button = document.querySelector("#more_button");
 
-more_button.addEventListener('click', () => {
-    let dropup_container = document.querySelector('.nav_dropup_container')
-    let nav_dropup = dropup_container.querySelector('.nav_dropup')
+more_button.addEventListener("click", () => {
+    let dropup_container = document.querySelector(".nav_dropup_container");
+    let nav_dropup = dropup_container.querySelector(".nav_dropup");
 
-    if (nav_dropup.dataset.state == 'active') {
-        nav_dropup.dataset.state = 'inactive'
+    if (nav_dropup.dataset.state == "active") {
+        nav_dropup.dataset.state = "inactive";
+    } else {
+        nav_dropup.dataset.state = "active";
     }
-    else {
-        nav_dropup.dataset.state = 'active'
-    }
-})
+});
 
 // nav bar more options menu lang dropup stuff?
 
-let lang_button = document.querySelector('#lang_select')
+let lang_button = document.querySelector("#lang_select");
 
-lang_button.addEventListener('click', toggleLangDropup)
+lang_button.addEventListener("click", toggleLangDropup);
 
 // dropup close events:
 
 // closes dropups when user clicks outside the dropup | ignores clicks in parent container
 function closeDropupOnOutsideClick(dropup_element, event) {
-    let container = dropup_element.parentElement
+    let container = dropup_element.parentElement;
 
-    if (container.contains(event.target)) { return }
+    if (container.contains(event.target)) { return; }
 
-    dropup_element.dataset.state = 'closed'
+    dropup_element.dataset.state = "closed";
 }
 
 // add click and mobile touch events for dropups
-document.querySelectorAll('#dropup').forEach(dropup => {
-    document.addEventListener('click', (event) => {
-        closeDropupOnOutsideClick(dropup, event)
-    })
-    document.addEventListener('touchstart', (event) => {
-        closeDropupOnOutsideClick(dropup, event)
-    })
+document.querySelectorAll("#dropup").forEach((dropup) => {
+    document.addEventListener("click", (event) => {
+        closeDropupOnOutsideClick(dropup, event);
+    });
+    document.addEventListener("touchstart", (event) => {
+        closeDropupOnOutsideClick(dropup, event);
+    });
 
-    iframe.addEventListener('load', () => {
-        iframe.contentWindow.addEventListener('click', (event) => {
-            closeDropupOnOutsideClick(dropup, event)
-        })
-        iframe.contentWindow.addEventListener('touchstart', (event) => {
-            closeDropupOnOutsideClick(dropup, event)
-        })
-    })
-})
+    iframe.addEventListener("load", () => {
+        iframe.contentWindow.addEventListener("click", (event) => {
+            closeDropupOnOutsideClick(dropup, event);
+        });
+        iframe.contentWindow.addEventListener("touchstart", (event) => {
+            closeDropupOnOutsideClick(dropup, event);
+        });
+    });
+});
