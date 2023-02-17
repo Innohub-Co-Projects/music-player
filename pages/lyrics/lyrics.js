5b20115f811738d91a37082ad49626f88108c0fd
Sankalp-G <sankalp.gawali@gmail.com>
2023-02-17 13:46:57 +0530

    fix formatting

--- pages/lyrics/lyrics.js
+++ pages/lyrics/lyrics.js
@@ -1,112 +1,111 @@
 // Set section header/title
-parent.setSectionTitle('Lyric Search');
+parent.setSectionTitle("Lyric Search");
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const result = document.getElementById('result')
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
 gapi.load("client", loadClient);
- 
+
 function loadClient() {
     gapi.client.setApiKey("AIzaSyCXd8-DzhjLSwKvOTPJzFJkZHiKvsPNAJs");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
-};
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest").then(
+        function () {
+            console.log("GAPI client loaded for API");
+        },
+        function (err) {
+            console.error("Error loading GAPI client for API", err);
+        }
+    );
+}
 // Make sure the client is loaded before calling this method.
 
-
-/// api URL ///
-const apiURL = 'https://api.lyrics.ovh';
-
-form.addEventListener('submit', e=> {
-    e.preventDefault();
-    searchValue = search.value.trim();
-
-    if(!searchValue){
-        alert("There is nothing to search")
-    }
-    else{ 
-        searchSong(searchValue)
-    }
-})
-
-// Key up event listner
-const searchOnKeyUp =() =>{
-    searchValue = search.value.trim();
-    searchSong(searchValue)
-}
 //search song 
 async function searchSong(searchValue){
+    const apiURL = 'https://api.lyrics.ovh';
     const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`)
     const data = await searchResult.json();
 
-    // console.log(finaldata)
     showData(data);
 }
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchValue = search.value.trim();
+
+    if (!searchValue) {
+        alert("There is nothing to search");
+    } else {
+        searchSong(searchValue);
+    }
+});
+
 //display final result in DO
-function showData(data){
-  
+function showData(data) {
     result.innerHTML = `
-   
     <ul class="song-list">
       ${data.data
-        .map(song=> `<li>
+          .map(
+              (song) => `<li>
                     <div>
                         <strong>${song.artist.name}</strong> -${song.title} 
                     </div>
                     <span data-artist="${song.artist.name}" data-songtitle="${song.title}"> get lyrics</span>
                 </li>`
-        )
-        .join('')}
+          )
+          .join("")}
     </ul>
   `;
-  document.getElementById('video').innerHTML = ''
-
-
+    document.getElementById("video").innerHTML = "";
 }
 
-
-
-
 //event listener in get lyrics button
-result.addEventListener('click', async e =>{
+result.addEventListener("click", async (e) => {
     const clickedElement = e.target;
 
     //checking clicked element is button or not
-    if (clickedElement.tagName === 'SPAN'){
-        const artist = clickedElement.getAttribute('data-artist');
-        const songTitle = clickedElement.getAttribute('data-songtitle');
+    if (clickedElement.tagName === "SPAN") {
+        const artist = clickedElement.getAttribute("data-artist");
+        const songTitle = clickedElement.getAttribute("data-songtitle");
 
-        await displayLyrics(artist, songTitle)
+        await displayLyrics(artist, songTitle);
     }
-})
+});
 
 const lyricOptions = {
-    method: 'GET',
+    method: "GET",
     headers: {
-        'X-RapidAPI-Key': 'c3cfdc2e25mshd7877d0aa3b4a97p1d1d33jsnb3badacf6fb8',
-        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
-    }
+        "X-RapidAPI-Key": "c3cfdc2e25mshd7877d0aa3b4a97p1d1d33jsnb3badacf6fb8",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+    },
 };
 
 function parseHTML(htmlString) {
-    temp = document.createElement('div');
+    temp = document.createElement("div");
     temp.innerHTML = htmlString;
     return temp;
 }
 
 async function topSearchResultID(searchTerm) {
-    data = await (await fetch('https://genius-song-lyrics1.p.rapidapi.com/search/?q=' + searchTerm + '&per_page=1&page=1', lyricOptions)).json();
+    data = await (
+        await fetch(
+            "https://genius-song-lyrics1.p.rapidapi.com/search/?q=" + searchTerm + "&per_page=1&page=1",
+            lyricOptions
+        )
+    ).json();
     return data.hits[0].result.id;
 }
 
 async function fetchLyrics(songTitle, artist) {
-    id = await topSearchResultID(songTitle + ' - ' + artist);
-    data = await (await fetch('https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=' + id + '&text_format=html', lyricOptions)).json();
-    
+    id = await topSearchResultID(songTitle + " - " + artist);
+    data = await (
+        await fetch(
+            "https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=" + id + "&text_format=html",
+            lyricOptions
+        )
+    ).json();
+
     html = parseHTML(data.lyrics.lyrics.body.html);
-    text_with_breaks = html.innerText.replace(/\r?\n/g, '<br/>');
+    text_with_breaks = html.innerText.replace(/\r?\n/g, "<br/>");
     return text_with_breaks;
 }
 
@@ -117,68 +116,62 @@ async function displayLyrics(artist, songTitle) {
     <h4 style="margin-bottom:30px;"><strong>${artist}</strong> - ${songTitle}</h4><ul>
     <div data-artist="${artist}" data-songtitle="${songTitle}"> get lyrics</div>
     <p style="margin-top:20px;">${lyrics}</p>
-`    
-    
+`;
 }
 
 //event listener in get song button
-result.addEventListener('click', e=>{
+result.addEventListener("click", (e) => {
     const clickedElement = e.target;
 
-    //checking clicked elemet is button or not
-    if (clickedElement.tagName === 'DIV'){
-        const artist = clickedElement.getAttribute('data-artist');
-        const songTitle = clickedElement.getAttribute('data-songtitle');
-        
+    //checking clicked element is button or not
+    if (clickedElement.tagName === "DIV") {
+        const artist = clickedElement.getAttribute("data-artist");
+        const songTitle = clickedElement.getAttribute("data-songtitle");
+
         execute(artist, songTitle);
     }
-    
-})
+});
 
-/*************************************************
- * 
- * 
- * 
- * 
- * 
- * ************************************************
- */
-const execute = (artist, songTitle)=>{
-    var pageToken = '';
+/////////
+
+const execute = (artist, songTitle) => {
+    var pageToken = "";
 
     var arr_search = {
-        "part": 'snippet',
-        "type": 'video',
-        "order": 'relevance',
-        "maxResults": 1,
-        "q": songTitle + artist
+        part: "snippet",
+        type: "video",
+        order: "relevance",
+        maxResults: 1,
+        q: songTitle + artist,
     };
- 
-    if (pageToken != '') {
+
+    if (pageToken != "") {
         arr_search.pageToken = pageToken;
     }
- 
-    return gapi.client.youtube.search.list(arr_search)
-    .then(function(response) {
-        // Handle the results here (response.result has the parsed body).
-        const listItems = response.result.items;
-        if (listItems) {
-            let output = `<h4 style="margin-bottom:30px;"><strong>${artist}</strong> - ${songTitle}</h4><ul>`;
- 
-            listItems.forEach(item => {
-                const videoId = item.id.videoId;
-                const videoTitle = item.snippet.title;
-                output += `
+
+    return gapi.client.youtube.search.list(arr_search).then(
+        function (response) {
+            // Handle the results here (response.result has the parsed body).
+            const listItems = response.result.items;
+            if (listItems) {
+                let output = `<h4 style="margin-bottom:30px;"><strong>${artist}</strong> - ${songTitle}</h4><ul>`;
+
+                listItems.forEach((item) => {
+                    const videoId = item.id.videoId;
+                    const videoTitle = item.snippet.title;
+                    output += `
                     <li><a data-fancybox href="https://www.youtube.com/watch?v=${videoId}"><img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" /></li>
                 `;
-            });
-            output += '</ul>';
- 
-            // Output list
-            document.getElementById('video').innerHTML = output
-           
+                });
+                output += "</ul>";
+
+                // Output list
+                document.getElementById("video").innerHTML = output;
+            }
+        },
+        function (err) {
+            console.error("Execute error", err);
         }
-    },
-    function(err) { console.error("Execute error", err); });
-    
-}
+
+    );
+};
--- pages/my-library/script.js
+++ pages/my-library/script.js
@@ -1,5 +1,5 @@
 // Set section header/title
-parent.setSectionTitle('My Library');
+parent.setSectionTitle("My Library");
 
 // ids are very important for a song object
 // they determine the img and audio src path for the song
@@ -11,21 +11,21 @@ class Song {
     }
 
     getSrc() {
-        return `./pages/my-library/audio/${this.id}.mp3`
+        return `./pages/my-library/audio/${this.id}.mp3`;
     }
 
     getPoster() {
-        return `./img/${this.id}.jpg`
+        return `./img/${this.id}.jpg`;
     }
 
     play() {
-        let song_thumbnail = "./pages/my-library/" + this.getPoster()
-        parent.playAudio(this.getSrc(), this.title, this.subtitle, song_thumbnail)
+        let song_thumbnail = "./pages/my-library/" + this.getPoster();
+        parent.playAudio(this.getSrc(), this.title, this.subtitle, song_thumbnail);
         current_song_id = this.id;
     }
 }
 
-// create Array 
+// create Array
 var songs = [
     new Song(1, "On My Way", "Alan Walker"),
     new Song(2, "Fade", "Alan Walker"),
@@ -41,84 +41,84 @@ var songs = [
     new Song(12, "Putt Jatt Da", "Putt Jatt Da"),
     new Song(13, "Baarishein", "Atif Aslam"),
     new Song(14, "Vaaste", "Dhvani Bhanushali"),
-    new Song(15, "Lut Gaye", "Jubin Nautiyal")
-]
+    new Song(15, "Lut Gaye", "Jubin Nautiyal"),
+];
 
-let song_elements = Array.from(document.getElementsByClassName('songItem'))
-let play_button_elements = Array.from(document.getElementsByClassName('playListPlay'))
+let song_elements = Array.from(document.getElementsByClassName("songItem"));
+let play_button_elements = Array.from(document.getElementsByClassName("playListPlay"));
 
-song_elements.forEach((element, i)=>{
-    element.getElementsByTagName('img')[0].src = songs[i].getPoster();
-    let title_text = `${songs[i].title} <br> <div class="subtitle">${songs[i].subtitle}</div>`
-    element.getElementsByTagName('h5')[0].innerHTML = title_text;
-})
+song_elements.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].getPoster();
+    let title_text = `${songs[i].title} <br> <div class="subtitle">${songs[i].subtitle}</div>`;
+    element.getElementsByTagName("h5")[0].innerHTML = title_text;
+});
 
-const makeAllPlays = () =>{
-    play_button_elements.forEach((element)=>{
-            element.classList.add('bi-play-circle-fill');
-            element.classList.remove('bi-pause-circle-fill');
-    })
-}
-const makeAllBackgrounds = () =>{
-    song_elements.forEach((element)=>{
-            element.style.background = "rgb(105, 105, 170, 0)";
-    })
-}
+const makeAllPlays = () => {
+    play_button_elements.forEach((element) => {
+        element.classList.add("bi-play-circle-fill");
+        element.classList.remove("bi-pause-circle-fill");
+    });
+};
+const makeAllBackgrounds = () => {
+    song_elements.forEach((element) => {
+        element.style.background = "rgb(105, 105, 170, 0)";
+    });
+};
 
 // active here just means that it has a pause icon instead of a play icon
 function setSongElementActive(song_element) {
     makeAllPlays();
-    let icon = song_element.querySelector('i')
-    icon.classList.remove('bi-play-circle-fill');
-    icon.classList.add('bi-pause-circle-fill');
+    let icon = song_element.querySelector("i");
+    icon.classList.remove("bi-play-circle-fill");
+    icon.classList.add("bi-pause-circle-fill");
     makeAllBackgrounds();
     song_element.style.background = "rgb(105, 105, 170, .1)";
 }
 
 function setSongElementActiveByID(song_id) {
-    let element = song_elements[song_id - 1]
-    setSongElementActive(element)
+    let element = song_elements[song_id - 1];
+    setSongElementActive(element);
 }
 
 var current_song_id = 0;
 
 function nextSong() {
     let next_index = current_song_id >= songs.length ? 0 : current_song_id;
-    let song = songs[next_index]
+    let song = songs[next_index];
     return song;
 }
 
 function previousSong() {
-    let prev_index = current_song_id - 2 < 0 ? songs.length - 1 : current_song_id - 2
-    let song = songs[prev_index]
+    let prev_index = current_song_id - 2 < 0 ? songs.length - 1 : current_song_id - 2;
+    let song = songs[prev_index];
     return song;
 }
 
-play_button_elements.forEach((element)=>{
-    element.addEventListener('click', (e)=>{
+play_button_elements.forEach((element) => {
+    element.addEventListener("click", (e) => {
         let song_id = e.target.id;
 
-        setSongElementActiveByID(song_id)
+        setSongElementActiveByID(song_id);
 
-        let song = songs[song_id - 1]
-        song.play()
-    })
-})
+        let song = songs[song_id - 1];
+        song.play();
+    });
+});
 
 // adds event listeners for all scroll buttons on the library page
 // assumes that the scrollable list is the second element of the parent container
-let button_containers = document.querySelectorAll('.btn_s')
-button_containers.forEach(button_container => {
-    let section_container = button_container.parentElement.parentElement
-    let list = section_container.children[1]
+let button_containers = document.querySelectorAll(".btn_s");
+button_containers.forEach((button_container) => {
+    let section_container = button_container.parentElement.parentElement;
+    let list = section_container.children[1];
 
-    let left_button = button_container.querySelector('#left_scroll')
-    let right_button = button_container.querySelector('#right_scroll')
+    let left_button = button_container.querySelector("#left_scroll");
+    let right_button = button_container.querySelector("#right_scroll");
 
-    left_button.addEventListener('click', ()=>{
+    left_button.addEventListener("click", () => {
         list.scrollLeft -= 330;
-    })
-    right_button.addEventListener('click', ()=>{
+    });
+    right_button.addEventListener("click", () => {
         list.scrollLeft += 330;
-    })
-})
+    });
+});
